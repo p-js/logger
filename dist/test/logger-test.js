5f6ed1c886c0418947a5857e46e81a5fd18e24ad
Darian Schramm <info@dvschramm.com>
2014-07-09 19:18:37 -0700

    grunt rig output of test

---
+++ dist/test/logger-test.js
@@ -0,0 +1,10 @@
+/*global Util, test, expect, ok, _*/
+test("logger test", function() {
+	var methods = ["debug", "log", "info", "error", "warn"],
+		logger = new(Logger)();
+	expect(methods.length);
+	_.each(methods, function(method) {
+		logger[method]("testing logger method:" + method);
+		ok(true, method + " worked");
+	});
+});
